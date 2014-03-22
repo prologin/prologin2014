5e535751ee416c687a2d69661369bf9a98fac268
Antoine Pietri <antoine.pietri@lrde.epita.fr>
2014-03-22 01:19:16 +0100

    add an action-construct base

---
+++ src/action-construct.cc
@@ -0,0 +1,44 @@
+#include "action-construct.hh"
+#include "constant.hh"
+#include "game.hh"
+#include "tools.hh"
+
+ActionConstruct::ActionConstruct(position pos, int range, int player)
+    : pos_(pos)
+    , range_(range)
+    , player_id_(player)
+{
+}
+
+ActionConstruct::ActionConstruct()
+    : pos_({-1, -1})
+    , range_(-1)
+    , player_id_(-1)
+{
+}
+
+int ActionConstruct::check(const GameState* st) const
+{
+    Cell* cell;
+    if (!(cell = st->get_map()->get_cell(pos_)))
+        return CASE_IMPOSSIBLE;
+
+    if (cell->get_type() != CASE_SIMPLE || cell->nb_wizards_total())
+        return CASE_UTILISEE;
+
+    // TODO: more checks
+
+    return OK;
+}
+
+void ActionConstruct::handle_buffer(utils::Buffer& buf)
+{
+    buf.handle(pos_);
+    buf.handle(range_);
+    buf.handle(player_id_);
+}
+
+void ActionConstruct::apply_on(GameState* st) const
+{
+    // TODO
+}
---
+++ src/action-construct.hh
@@ -0,0 +1,28 @@
+#ifndef ACTION_CONSTRUCT_HH_
+# define ACTION_CONSTRUCT_HH_
+
+# include <rules/action.hh>
+
+# include "game.hh"
+# include "constant.hh"
+
+class ActionConstruct : public rules::Action<GameState>
+{
+    public:
+        ActionConstruct(position pos, int range, int player);
+        ActionConstruct();
+
+        virtual int check(const GameState* st) const;
+        virtual void handle_buffer(utils::Buffer& buf);
+        virtual void apply_on(GameState* gameState) const;
+
+        uint32_t player_id() const { return player_id_; }
+        uint32_t id() const { return ID_ACTION_CONSTRUCT; }
+
+    private:
+        position pos_;
+        int range_;
+        int player_id_;
+};
+
+#endif /* !ACTION_CONSTRUCT_HH_ */
