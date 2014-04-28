c9c7311edab38f5b190662717048fdda724fe847
Antoine Pietri <antoine.pietri@lrde.epita.fr>
2014-04-28 05:28:03 +0200

    add GPL license

--- src/action-ack.cc
+++ src/action-ack.cc
@@ -1,3 +1,22 @@
+/*
+** Copyright (c) 2014 Mélanie Godard <melanie.godard@prologin.org>
+** Copyright (c) 2014 Antoine Pietri <antoine.pietri@prologin.org>
+** Copyright (c) 2014 Association Prologin <info@prologin.org>
+**
+** prologin2014 is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** prologin2014 is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with prologin2014.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "action-ack.hh"
 
 ActionAck::ActionAck(int player)
