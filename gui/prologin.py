df9ace7fa8fd076e48f894aa8853a3df0690faf7
Pierre-Marie de Rodat <pmderodat@kawie.fr>
2014-05-09 11:28:13 +0200

    gui: fix the stechec integration to provide a _valid_ fake "argv"

--- gui/prologin.py
+++ gui/prologin.py
@@ -13,7 +13,7 @@ game_state_reader = state_reader.StechecReader()
 window = window.Window(game_state_reader)
 gfx_thread = threading.Thread(target=window.run)
 
-settings.load(['gui.so'])
+settings.load([])
 
 def partie_debut():
     gfx_thread.start()
